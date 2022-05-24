01101ddee17e2582ea41fe8af9bf70d0acd839c4
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-05-24 12:04:01 +0200

    cleanup gitlab deploy script

--- deployments/gitlab/deploy.sh
+++ deployments/gitlab/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
+set -eo pipefail
 
 kubectl apply -k .
 helm upgrade --install gitlab gitlab/gitlab --namespace=gitlab --version=5.10.2 --values=values.yaml 
-helm upgrade --install gitlab mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
+helm upgrade --install gitlab-redis bitnami/redis --namespace=gitlab --version=16.9.11 --values=values.redis.yaml
+helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
