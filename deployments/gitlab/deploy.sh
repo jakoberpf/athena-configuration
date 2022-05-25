7b4f16689cdcdc9b9c00175dab6ab5436ce90dab
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-05-25 20:06:14 +0200

    update

--- deployments/gitlab/deploy.sh
+++ deployments/gitlab/deploy.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 kubectl apply -k .
 helm upgrade --install gitlab gitlab/gitlab --namespace=gitlab --version=5.10.2 --values=values.yaml 
-helm upgrade --install gitlab-redis bitnami/redis --namespace=gitlab --version=16.9.11 --values=values.redis.yaml
-helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
+
+# helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
