b524855126efc7d443122b324f407ddda217a960
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-05-26 16:43:29 +0200

    update deployment scripts

--- deployments/gitpod/deploy.sh
+++ deployments/gitpod/deploy.sh
@@ -11,36 +11,35 @@ done
 workers+=$(kubectl get nodes -o custom-columns=NAME:.metadata.name --no-headers -l node-role.kubernetes.io/worker)
 for node in ${workers}; do
     echo "Labeling worker node ${node}"
-    kubectl label nodes ${node} gitpod.io/workload_meta=true
-    kubectl label nodes ${node} gitpod.io/workload_ide=true
-    kubectl label nodes ${node} gitpod.io/workload_workspace_services=true
-    kubectl label nodes ${node} gitpod.io/workload_workspace_regular=true
-    kubectl label nodes ${node} gitpod.io/workload_workspace_headless=true
+    kubectl label nodes ${node} gitpod.io/workload_meta=true --overwrite
+    kubectl label nodes ${node} gitpod.io/workload_ide=true --overwrite
+    kubectl label nodes ${node} gitpod.io/workload_workspace_services=true --overwrite
+    kubectl label nodes ${node} gitpod.io/workload_workspace_regular=true --overwrite
+    kubectl label nodes ${node} gitpod.io/workload_workspace_headless=true --overwrite
 done
 
 kubectl apply -k .
 kubens gitpod
-# kubectl kots install gitpod
 
 docker build . -t gitpod-installer
 
-docker run -it --rm \
-  --name gitpod-installer \
-  --volume "$(pwd)"/installer:/installer \
-  gitpod-installer gitpod-installer init >> gitpod.config.yaml
+# docker run -it --rm \
+#   --name gitpod-installer \
+#   --volume "$(pwd)"/installer:/installer \
+#   gitpod-installer gitpod-installer init >> installer/config.yaml
 
 docker run -it --rm \
   --name gitpod-installer \
   --volume "$(pwd)"/installer:/installer \
-  gitpod-installer gitpod-installer validate config --config /installer/gitpod.config.yaml
+  gitpod-installer gitpod-installer validate config --config /installer/config.yaml
 
 docker run -it --rm \
   --name gitpod-installer \
   --volume "$(pwd)"/installer:/installer \
   --volume "$(pwd)"/../../.kube:/kube \
-  gitpod-installer gitpod-installer validate cluster --kubeconfig /kube/admin.conf --config /installer/gitpod.config.yaml
+  gitpod-installer gitpod-installer validate cluster --kubeconfig /kube/admin.conf --config /installer/config.yaml
 
 docker run -it --rm \
   --name gitpod-installer \
   --volume "$(pwd)"/installer:/installer \
-  gitpod-installer gitpod-installer render --config /installer/gitpod.config.yaml > installer/gitpod.deployment.yaml
+  gitpod-installer gitpod-installer render --config /installer/config.yaml > installer/deployment.yaml
