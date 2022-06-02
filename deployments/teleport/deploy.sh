47e91ba75c62827660e76b186ed7a4f111a5940b
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-06-02 20:33:58 +0200

    update teleport deployment

--- deployments/teleport/deploy.sh
+++ deployments/teleport/deploy.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 kubectl apply -k .
-helm upgrade --install teleport /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/teleport --namespace=teleport # --values=values.yaml
+helm upgrade --install teleport /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/teleport --namespace=teleport --values=values.yaml
