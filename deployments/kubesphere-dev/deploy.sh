c0a84c7b16e2443360086dd708d9d313676453b1
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-08 23:38:46 +0200

    kubesphere cleanup

---
+++ deployments/kubesphere-dev/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+kubectl apply -f 
+
+kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
