a6648cf2ac5d5e287033f2bcc960ae494b9020cb
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-09 01:44:41 +0200

    seperate dns into http and https backend

--- dns/dns.tf
+++ dns/dns.tf
@@ -6,13 +6,39 @@ module "dns" {
     remote     = remote
   }
 
-  for_each = toset(local.domains)
+  for_each = toset(local.domains_http)
 
   domains = [
     each.key
   ]
+
   host = "10.110.180.110"
   port = 30080
+
+  https_enabled = false
+
+  cloudflare_email        = var.cloudflare_email
+  cloudflare_zone_id      = var.cloudflare_zone_id
+  cloudflare_token        = var.cloudflare_token
+  cloudflare_record_value = "primary.gateway.dns.erpf.de"
+}
+
+module "dns_for_https" {
+  source = "/Users/jakoberpf/Code/jakoberpf/terraform/modules/erpf/caddy-ingress" # "jakoberpf/gateway-ingress/erpf"
+
+  providers = {
+    cloudflare = cloudflare
+    remote     = remote
+  }
+
+  for_each = toset(local.domains_https)
+
+  domains = [
+    each.key
+  ]
+
+  host = "10.110.180.110"
+  port = 30443
 
   cloudflare_email        = var.cloudflare_email
   cloudflare_zone_id      = var.cloudflare_zone_id
@@ -21,11 +47,17 @@ module "dns" {
 }
 
 locals {
-  domains = [
+  domains_http = [    
+    "gitlab.dev.erpf.de",
+    "gitpod.dev.erpf.de",
+    "teleport.dev.erpf.de",
+    "anonaddy.dev.erpf.de"
+  ]
+  domains_https = [    
+    "bitwarden.erpf.de",
+    "vaultwarden.erpf.de",
     "bashhub.dev.erpf.de",
     "vaultwarden.dev.erpf.de",
     "bitwarden.dev.erpf.de",
-    "gitlab.dev.erpf.de",
-    "gitpod.dev.erpf.de",
   ]
 }
