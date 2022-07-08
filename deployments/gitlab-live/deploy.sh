2bad874771ebef8e449e392dc4bb942b4cd88132
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-08 16:02:59 +0200

    duplicate gitlab for dev and live instance

---
+++ deployments/gitlab-live/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+kubectl apply -k .
+helm upgrade --install gitlab gitlab/gitlab --namespace=gitlab --version=5.10.2 --values=values.yaml 
+
+# helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
