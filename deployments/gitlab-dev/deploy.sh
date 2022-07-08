71070ef83eb19044fdc882f99399e65a96638edd
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-08 17:26:09 +0200

    add line seperation

--- deployments/gitlab-dev/deploy.sh
+++ deployments/gitlab-dev/deploy.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 kubectl apply -k .
-helm upgrade --install gitlab gitlab/gitlab --namespace=gitlab --version=5.10.2 --values=values.yaml 
+
+helm upgrade --install gitlab gitlab/gitlab --namespace=gitlab-dev --version=5.10.2 --values=values.yaml 
 
 # helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
