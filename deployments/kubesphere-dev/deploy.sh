71ec9efeb88950a0a243bdf228df28a1707c24ea
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-08 23:45:30 +0200

    upgrade deployment script for kubesphere

--- deployments/kubesphere-dev/deploy.sh
+++ deployments/kubesphere-dev/deploy.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
-kubectl apply -f 
+kubectl apply -f kubesphere-installer.yaml
+kubectl apply -f cluster-configuration.yaml
 
 kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
