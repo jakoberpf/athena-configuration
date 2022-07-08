27aba0811a45e7638f3ac0cbeb9e410baee6cea9
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-09 01:08:44 +0200

    update url and vaultwarden version

--- deployments/vaultwarden-dev/deploy.sh
+++ deployments/vaultwarden-dev/deploy.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 kubectl apply -k .
-helm upgrade --install vaultwarden-dev erpf/vaultwarden --namespace=vaultwarden-dev --version=0.1.8 --values=values.yaml
+helm upgrade --install vaultwarden-dev erpf/vaultwarden --namespace=vaultwarden-dev --version=0.1.9 --values=values.yaml
--- deployments/vaultwarden-live/deploy.sh
+++ deployments/vaultwarden-live/deploy.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 kubectl apply -k .
-helm upgrade --install vaultwarden-live erpf/vaultwarden --namespace=vaultwarden-live --version=0.1.8 --values=values.yaml
+helm upgrade --install vaultwarden-live erpf/vaultwarden --namespace=vaultwarden-live --version=0.1.9 --values=values.yaml
