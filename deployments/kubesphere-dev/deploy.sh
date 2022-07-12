fda46753f1f0787f1db0588aec65c1713384b60f
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-12 12:40:03 +0200

    update deployment script for kubersphere

--- deployments/kubesphere-dev/deploy.sh
+++ deployments/kubesphere-dev/deploy.sh
@@ -2,9 +2,16 @@
 
 set -eo pipefail
 
+kubectl apply -f kubesphere-installer.yaml
+
+sleep 5 # give some time to digest CRDs creation
+
+kubectl apply -f cluster-configuration.yaml
+
+sleep 1
+
 kubectl apply -k .
 
-kubectl apply -f kubesphere-installer.yaml
-kubectl apply -f cluster-configuration.yaml
+sleep 20 # give some time allow container creation
 
 kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
