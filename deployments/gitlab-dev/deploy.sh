01343f0d9aec1deb78f50dff11b208e2ab153e0c
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-17 14:37:06 +0200

    deploy local chart

--- deployments/gitlab-dev/deploy.sh
+++ deployments/gitlab-dev/deploy.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 kubectl apply -k .
 kubens gitlab-dev
 
-# helm upgrade --install gitlab-dev gitlab/gitlab --namespace=gitlab-dev --version=5.10.2 --values=values.yaml 
+helm upgrade --install gitlab-dev /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/gitlab --namespace=gitlab-dev --values=values.yaml
 
 # helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
