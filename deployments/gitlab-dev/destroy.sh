0c7c36e74973363d25afc0a51df41d9236834db6
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-18 10:26:39 +0200

    add destroy script for faster dev

---
+++ deployments/gitlab-dev/destroy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+namespace=gitlab-dev
+lpname=longhorn-provisioner
+# set -eo pipefail
+
+kubens $namespace
+
+helm uninstall gitlab-dev
+
+# kubectl delete namespace $namespace
+
+kubectl delete serviceaccount $lpname
+kubectl delete clusterrole $lpname
+kubectl delete clusterrolebinding $lpname
