99fecebc82418b52473ee971fa0a47e11c1b2c0c
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-18 21:30:57 +0200

    update gitlab version

--- deployments/gitlab-dev/deploy.sh
+++ deployments/gitlab-dev/deploy.sh
@@ -8,6 +8,6 @@ kubens gitlab-dev
 
 helm upgrade --install gitlab-umbrella /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/gitlab --namespace=gitlab-dev --values=values-umbrella.yaml
 
-helm upgrade --install gitlab-dev gitlab/gitlab --version=5.10.2 --namespace=gitlab-dev --values=values.yaml
+helm upgrade --install gitlab-dev gitlab/gitlab --version=6.1.2 --namespace=gitlab-dev --values=values.yaml
 
 # helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
