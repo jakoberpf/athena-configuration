79ec153bd0e7ee0faf0aca65de78e10caf367fb7
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-18 20:43:55 +0200

    remove longhorn api and cleanup ressources

--- deployments/gitlab-dev/volumes.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-kubectl port-forward services/longhorn-frontend 8080:http -n longhorn-system
-
-docker run --rm --mount type=bind,source="$PWD"/config.yaml,dst=/longhorn/config.yaml docker.io/jakoberpf/longhorn-api
