47a586aac4f80b4ff95d7fefaef65aeb6f03a907
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-07-25 16:00:51 +0200

    fix deployment script

--- deployments/gitlab-dev/deploy.sh
+++ deployments/gitlab-dev/deploy.sh
@@ -6,8 +6,8 @@ kubectl apply -k .
 
 kubens gitlab-dev
 
-helm upgrade --install gitlab-umbrella /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/gitlab --namespace=gitlab-dev --values=values-umbrella.yaml
+helm upgrade --install gitlab-umbrella-dev /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/gitlab --namespace=gitlab-dev --values=values-umbrella.yaml
 
-helm upgrade --install gitlab-dev gitlab/gitlab --version=6.1.2 --namespace=gitlab-dev --values=values.yaml
+# helm upgrade --install gitlab-dev gitlab/gitlab --version=6.1.2 --namespace=gitlab-dev --values=values.yaml
 
 # helm upgrade --install gitlab-mattermost mattermost/mattermost-team-edition --namespace=gitlab --version=6.6.3 --values=values.mattermost.yaml
