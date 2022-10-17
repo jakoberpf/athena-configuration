95bff4910556681ac23f98dde8d99a1d58b9b5ca
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-10-17 20:57:51 +0200

    update vaultwarden live deployment

--- deployments/vaultwarden-live/deploy.sh
+++ deployments/vaultwarden-live/deploy.sh
@@ -3,4 +3,7 @@
 set -eo pipefail
 
 kubectl apply -k .
-helm upgrade --install vaultwarden-live erpf/vaultwarden --namespace=vaultwarden-live --version=0.1.9 --values=values.yaml
+kubectl scale deployment -n vaultwarden-live --replicas=0 vaultwarden-live
+sleep 3
+helm upgrade --install vaultwarden-live erpf/vaultwarden --namespace=vaultwarden-live --version=0.1.12 --values=values.yaml
+kubectl scale deployment -n vaultwarden-live --replicas=1 vaultwarden-live
